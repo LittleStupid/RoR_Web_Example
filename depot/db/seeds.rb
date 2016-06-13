131972334478c46629e648b4dc84ab4248276c6d
LittleStupid <lttlstpd@live.com>
2016-06-13 10:27:20 +0800

    add another test product when init.

--- depot/db/seeds.rb
+++ depot/db/seeds.rb
@@ -1,6 +1,11 @@
 Product.delete_all
 
-Product.create!( title: 'Ruby Book III',
-  description: 'book description',
-  image_url: 'ruby.jpg',
-  price: 49.95 )
+Product.create!(title: 'Ruby Book III',
+                description: 'book description',
+                image_url: 'ruby.jpg',
+                price: 49.95)
+
+Product.create!(title: 'Ruby Book II',
+                description: 'book description',
+                image_url: 'ruby.jpg',
+                price: 49.95)
