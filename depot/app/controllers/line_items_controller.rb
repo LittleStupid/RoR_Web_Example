e2ec27045906d9eb173d5c0d1b1ba0ae58af3623
LittleStupid <lttlstpd@live.com>
2016-06-14 18:49:32 +0800

    change cart with the help of ajax.

--- depot/app/controllers/line_items_controller.rb
+++ depot/app/controllers/line_items_controller.rb
@@ -2,7 +2,7 @@ class LineItemsController < ApplicationController
     include CurrentCart
     before_action :set_cart, only: [:create]
     before_action :set_line_item, only: [:show, :edit, :update, :destroy]
-
+    
     # GET /line_items
     # GET /line_items.json
     def index
@@ -32,6 +32,7 @@ class LineItemsController < ApplicationController
         respond_to do |format|
             if @line_item.save
                 format.html { redirect_to store_url }
+                format.js
                 format.json { render :show, status: :created, location: @line_item }
             else
                 format.html { render :new }
